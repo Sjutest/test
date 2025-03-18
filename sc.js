a2321e347ba6f9292abf20346e94171a97c9bc7c
Sjutest <carmenavlad2009@gmail.com>
2025-03-18 19:24:38 +0200

    Update and rename sc.js to script.js

--- sc.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('showMore').addEventListener('click', function() {
-    alert('Aici vor fi mai multe informații despre proiectele mele!');
-});
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mesajul tău a fost trimis!');
-});
